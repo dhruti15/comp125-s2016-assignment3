f2a856f4089a0164f0175582976d34693ab3b346
User <user@Users-MacBook-Air.local>
2016-08-05 23:07:01 -0400

    ajax upadtion

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,36 @@
 (function(){
     "use strict";
     
+/**
+ * Function Ajax Property
+ * 
+ */
+function fetchJSONFile(path, callback) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState === 4) {
+                if (httpRequest.status === 200) {
+                    var data = JSON.parse(httpRequest.responseText);
+                    if (callback) callback(data);
+                }
+            }
+        };
+        httpRequest.open('GET', path);
+        httpRequest.send();
+    }
+         // this requests the file and executes a callback with the parsed result once
+    //   it is available
+    fetchJSONFile('content.json', function (data) {
+        // do something with your data
+        console.log(data);
 
+        var paragraphIDs = ["paraone", "paratwo", "parathree", "parafour", "parafive","parasix","paraseven","paraeig","paranine","paraten","paraele","paratwe","parathi","paraft","parafif","parasis","dh","parao"];
+        for (var i = 0; i < paragraphIDs.length; i++) {
+            var p = document.getElementById(paragraphIDs[i]);
+            p.innerHTML = data.content[i].data;
+
+        }
+    });
 
      
 
